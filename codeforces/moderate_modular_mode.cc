bbbf3692c726a78466c196f498ed7e192b30102a
Vladimir Kim <zjgkkn@gmail.com>
2025-02-23 18:41:49 +0100

    Update

---
+++ codeforces/moderate_modular_mode.cc
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using std::abs;
+using std::endl;
+using std::map;
+using std::max;
+using std::min;
+using std::pair;
+using std::priority_queue;
+using std::queue;
+using std::set;
+using std::sort;
+using std::sqrt;
+using std::stack;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+int main(int /*argc*/, char* /*argv*/[]) {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int t = 0;
+  std::cin >> t;
+  while ((t--) != 0) {
+    int64_t x = 0;
+    int64_t y = 0;
+    std::cin >> x >> y;
+
+    if (x == y) {
+      std::cout << x << "\n";
+      continue;
+    }
+
+    if (x > y) {
+      std::cout << x + y << "\n";
+      continue;
+    }
+
+    std::cout << y - ((y % x) / 2) << "\n";
+  }
+
+  return 0;
+}
