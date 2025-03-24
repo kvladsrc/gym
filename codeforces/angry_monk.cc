4ab06f0e5e989e1c92ab8f8baf16fcc982207b45
Vladimir Kim <zjgkkn@gmail.com>
2025-03-24 21:42:46 +0100

    Update

---
+++ codeforces/angry_monk.cc
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using std::abs;
+using std::endl;
+using std::map;
+using std::max;
+using std::min;
+using std::pair;
+using std::priority_queue;
+using std::queue;
+using std::set;
+using std::sort;
+using std::sqrt;
+using std::stack;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+int main(int /*argc*/, char * /*argv*/[]) {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int t = 0;
+  std::cin >> t;
+  while ((t--) != 0) {
+    int n = 0;
+    int k = 0;
+    std::cin >> n >> k;
+
+    vector<int> a(k);
+    for (auto &i : a) {
+      std::cin >> i;
+    }
+
+    sort(a.begin(), a.end());
+    a.pop_back();
+
+    int res = 0;
+    for (auto i : a) {
+      res += i - 1;
+      res += i;
+    }
+
+    std::cout << res << "\n";
+  }
+
+  return 0;
+}
