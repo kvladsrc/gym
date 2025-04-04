ea3cc1e2517009b41639a603896053768751b147
Vladimir Kim <zjgkkn@gmail.com>
2025-04-05 00:01:42 +0200

    Update

---
+++ codeforces/gardener_and_tree.cc
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using std::abs;
+using std::endl;
+using std::map;
+using std::max;
+using std::min;
+using std::pair;
+using std::priority_queue;
+using std::queue;
+using std::set;
+using std::sort;
+using std::sqrt;
+using std::stack;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+using graph = vector<set<int>>;
+
+int main(int /*argc*/, char* /*argv*/[]) {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int t = 0;
+  std::cin >> t;
+  while ((t--) != 0) {
+    int n = 0;
+    int k = 0;
+    std::cin >> n >> k;
+
+    graph g(n);
+
+    for (int i = 1; i < n; ++i) {
+      int a = 0;
+      int b = 0;
+      std::cin >> a >> b;
+      a--;
+      b--;
+      g[a].insert(b);
+      g[b].insert(a);
+    }
+
+    vector<int> leafs;
+    vector<int> new_leafs;
+
+    for (int i = 0; i < n; ++i) {
+      if (g[i].size() <= 1) {
+        leafs.push_back(i);
+      }
+    }
+
+    int res = n;
+    for (int i = 0; i < k; ++i) {
+      if (leafs.empty()) {
+        break;
+      }
+
+      res -= leafs.size();
+
+      for (auto l : leafs) {
+        if (g[l].empty()) {
+          continue;
+        }
+
+        auto p = *g[l].begin();
+        g[p].erase(l);
+        if (g[p].size() == 1) {
+          new_leafs.push_back(p);
+        }
+      }
+
+      leafs = new_leafs;
+      new_leafs.clear();
+    }
+
+    std::cout << res << "\n";
+  }
+
+  return 0;
+}
