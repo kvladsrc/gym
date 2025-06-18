fad11cee3a667c6de804bc11ca5e7c856a3c9f8e
Владимир Ким <zjgkkn@bonfire.lit>
2025-06-18 23:24:05 +0200

    Update

---
+++ codeforces/zero_path.cc
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using std::abs;
+using std::endl;
+using std::map;
+using std::max;
+using std::min;
+using std::pair;
+using std::priority_queue;
+using std::queue;
+using std::set;
+using std::sort;
+using std::sqrt;
+using std::stack;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+int main(int /*argc*/, char* /*argv*/[]) {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  /*
+   * Let S(x) is the sum of squares on path x. If exists p1, S(p1) > 0
+   * and p2, S(p2) < 0, and n + m is even -> exists p3, S(p3) =
+   * 0. Because every path can be transormed to another by simple one
+   * sqare replacement and every such transformation change sum by 2.
+   */
+
+  return 0;
+}
